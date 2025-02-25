d984a31354d59208c3e9dbe1f7bfc810be0bc225
PBW <bottlerstudy3123@gmail.com>
2025-02-25 23:20:41 +0900

    feat(#30) : 로고 제작

--- src/components/shared/logo/styles.js
+++ src/components/shared/logo/styles.js
@@ -2,13 +2,6 @@ import styled, { css } from 'styled-components';
 
 export const LogoBox = styled.div`
 
-  ${({ size }) => {
-    return css`
-      width: ${size};
-      height: ${size};
-    `;
-  }}
-
   img {
     width: 100%;
     height: 100%;
