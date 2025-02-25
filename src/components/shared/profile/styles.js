912426806138fc69ccb6eb538c07b8cc2ea0f373
전민정 <minj_1203@naver.com>
2025-02-25 18:03:24 +0900

    feat(#34): nickname 공통 컴포넌트 생성

--- src/components/shared/profile/styles.js
+++ src/components/shared/profile/styles.js
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components';
+import vars from '../../../styles/vars';
 
+// profileImg style
 export const ImgBox = styled.div`
   background-color: white;
   display: flex;
@@ -21,4 +23,38 @@ export const ImgBox = styled.div`
     height: 100%;
     object-fit: cover;
   }
+`;
+
+// nickname style
+const fontSizeStyles = {
+  large: vars.fontSize['1.25x'],
+  medium: vars.fontSize['0.875x'],
+  small: vars.fontSize['0.75x'],
+  default: vars.fontSize['1x'],
+};
+
+const fontColorStyles = {
+  black: vars.colors.black,
+  red: vars.colors.red,
+  blue: vars.colors.blue[500],
+};
+
+export const NicknameBox = styled.div`
+  display: flex;
+  text-align: center;
+  margin: 4px;
+  padding: 4px;
+
+  ${({ size, color, fontWeight }) => {
+    const fontSize = fontSizeStyles[size] || fontSizeStyles.default;
+    const fontColor = fontColorStyles[color] || fontColorStyles.black;
+    const fontWeightStyle =
+      vars.fontWeight[fontWeight] || vars.fontWeight.medium;
+
+    return css`
+      font-size: ${fontSize};
+      color: ${fontColor};
+      font-weight: ${fontWeightStyle};
+    `;
+  }}
 `;
