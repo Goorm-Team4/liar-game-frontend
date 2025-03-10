82576e10f4fc81b3f7c94ad41872fe810e46ee9e
ssuzn <tnwls_723@naver.com>
2025-03-10 21:53:33 +0900

    feat(#78): 공통 모달 컴포넌트 제작

---
+++ src/components/shared/modal/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import vars from "@/styles/vars";
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(4px);
+  z-index: 1;
+`;
+
+export const ModalContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 350px;
+  height: 300px;
+  border-radius: ${vars.borderRadius['4x']};
+  background-color: ${vars.colors['white']};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 30px;
+  padding: 30px;
+`;
+
+export const TitleContainer = styled.h2`
+  font-size: ${vars.fontSize['1.5x']};
+  font-weight: ${vars.fontWeight['semibold']};
+  text-align: center;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 12px;
+  flex-direction: column;
+  align-items: center;
+`;
--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
+import Modal from '@/components/shared/modal';
 
 const test = () => {
-  return <TestContainer></TestContainer>;
+  return <TestContainer>
+    <Modal title="플레이어 퇴장" onClose={() => {}} buttons={[{ label: "다시하기", onClick: () => console.log("clicked") }, { label: "홈으로", color: "blue", onClick: () => console.log("clicked") }]} />
+    <h1>Hello, World!</h1>
+    <p>This is a test modal.</p>
+  </TestContainer>;
 };
 
 export default test;
