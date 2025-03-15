294a05f774181769de3470f95e744baf2662ba9c
전민정 <minj_1203@naver.com>
2025-03-15 18:55:17 +0900

    feat(#87): 로그인 ui 수정

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,14 @@
-import styled from 'styled-components';
+import {
+  PageContainer,
+  SectionContainer,
+  TitleContainer,
+  ButtonContainer,
+  KakaoButton,
+} from './styles';
 import { useNavigate } from 'react-router-dom';
-import kakaoLoginLogo from '@/assets/images/kakao_login_medium_narrow.png';
-import { useEffect } from 'react';
+import kakaoLoginLogo from '@/assets/images/kakaoLogin.svg';
+import Button from '@/components/shared/button';
+import { ROUTE } from '@/constants/router';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -11,15 +18,21 @@ const Login = () => {
     window.location.href = kakaoLoginURL;
   }
 
+  const handleGuestLogin = () => {
+    navigate(ROUTE.auth);
+  };
+
   return (
     <PageContainer>
       <SectionContainer>
-        <TitleContainer>라이어게임</TitleContainer>
+        <TitleContainer>LiarTalk</TitleContainer>
         <ButtonContainer>
           <KakaoButton onClick={() => kakaoLoginHandle()}>
             <img src={kakaoLoginLogo} />
           </KakaoButton>
-          <Button onClick={() => navigate('/auth')}>게스트로 시작하기</Button>
+          <Button size="large" color="gray" onClick={handleGuestLogin}>
+            게스트로 시작하기
+          </Button>
         </ButtonContainer>
       </SectionContainer>
     </PageContainer>
@@ -27,70 +40,3 @@ const Login = () => {
 };
 
 export default Login;
-
-const PageContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100vh;
-`;
-
-const SectionContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-  width: 100%;
-  padding: 9vh;
-`;
-
-const TitleContainer = styled.div`
-  font-weight: 700;
-  font-size: 4.5vh;
-  text-align: center;
-  margin-top: 7vh;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 3vh;
-  width: 90%;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Button = styled.button`
-  padding: 0;
-  width: 190px;
-  height: 44px;
-  line-height: 44px;
-  background-color: #f1f1f1;
-  border: 1px solid transparent;
-  border-radius: 3px;
-  font-size: 16px;
-  font-weight: bold;
-  text-align: center;
-  cursor: pointer;
-
-  &:hover {
-    box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2);
-  }
-`;
-
-const KakaoButton = styled.button`
-  padding: 0;
-  width: min-content;
-  height: min-content;
-  color: #783c00;
-  background-color: #ffeb00;
-  border: 1px solid transparent;
-  border-radius: 3px;
-  font-size: 16px;
-  font-weight: bold;
-  text-align: center;
-  cursor: pointer;
-
-  &:hover {
-    box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2);
-  }
-`;
